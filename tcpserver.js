4801b51a7c066ee188ed34442280925e76af7847
Sukhrattan Singh <sukhrattansingh@Sukhrattans-Mac-mini.local>
2026-03-05 19:36:10 +0530

    Adding TCP server file to git

---
+++ tcpserver.js
@@ -0,0 +1,24 @@
+import * as net from 'net';
+const server = net.createServer();
+server.on('error',(err)=>{throw err;});
+server.on('connection',onConnection);
+server.listen({host: '127.0.0.1',port:1234})
+
+
+function onConnection(socket){
+    console.log("New Connection : ",socket.remoteAddress , socket.remotePort);
+
+    socket.on('end',()=>{
+        console.log("EOF (End of File");
+    })
+
+    socket.on('data',(data)=>{
+        console.log("data : ",data);
+        socket.write(data);
+
+        if(data.includes('q')){
+            socket.end();
+        }
+    })
+
+}
